b7cf6442fb3a367020f5313031a68eb4ad5f3ad2
Franklin Mathurin <kainingem@gmail.com>
2018-06-30 23:28:35 +0100

    More images added to slideshow, adjusted content for mobile and tablet view

--- site/web/app/themes/pentzero/resources/views/index.blade.php
+++ site/web/app/themes/pentzero/resources/views/index.blade.php
@@ -16,6 +16,9 @@
     <div class="slideshow__item slideshow__item-5"></div>
     <div class="slideshow__item slideshow__item-6"></div>
     <div class="slideshow__item slideshow__item-7"></div>
+    <div class="slideshow__item slideshow__item-9"></div>
+    <div class="slideshow__item slideshow__item-10"></div>
+    <div class="slideshow__item slideshow__item-11"></div>
   </div>
   <div class="module-media-c-wrapper">
     <div class="module-media-card">
@@ -37,19 +40,10 @@
 </div>
 
 <div class="page-footer">
-  <div class="page-footer-content-wrap">
+  <div class="page-footer_content-wrap">
     <div class="container">
       <div class="row">
-        <div class="col-12 col-md-6 col-lg-4">
-          <div class="link-box link-box--contact-us">
-            <a href="/contact-us/" class="opacity-hover"><div class="global-image-wrapper"><img src="@asset('images/diddy-phone-1280x960.png')" class="img-fluid"/></div></a>
-            <h4 class="link-box-title link-box-title--contact-us">Contact Us</h4>
-            <div class="link-box-text">Send us a message</div>
-            <a href="/contact-us/" class="link-box-btn">Contact Us</a>
-          </div>
-        </div>
-
-        <div class="col-12 col-lg-4">
+        <div class="col-12 col-lg-4 order-lg-2">
           <div class="no-link-box no-link-box--about">
             <div class="no-link-box-center">
               <h3 class="no-link-box-title no-link-box-title--about">About</h3>
@@ -59,14 +53,40 @@
           </div>
         </div>
 
-        <div class="col-12 col-md-6 col-lg-4">
-          <div class="link-box link-box--submissions">
-            <a href="/submissions/" class="opacity-hover"><div class="global-image-wrapper"><img src="@asset('images/lohan-1280x960.png')" class="img-fluid"/></div></a>
-            <h4 class="link-box-title link-box-title--submissions">Send in your content</h4>
-            <div class="link-box-text">Share exclusive content with us</div>
-            <a href="/submissions/" class="link-box-btn">Send in your content</a>
+        <div class="col-12 col-lg-4 order-lg-1">
+          <div class="link-box link-box--contact-us">
+            <div class="row">
+              <div class="col-6 col-lg-12">
+                <a href="/contact-us/" class="opacity-hover"><div class="global-image-wrapper"><img src="@asset('images/diddy-phone-1280x960.png')" class="img-fluid"/></div></a>
+              </div>
+              <div class="col-6 col-lg-12">
+                <h4 class="link-box-title link-box-title--contact-us">Contact Us</h4>
+                <div class="link-box-text">Send us a message</div>
+                <a href="/contact-us/" class="link-box-btn">Contact Us</a>
+              </div>
+            </div>
           </div>
         </div>
+
+        <div class="col-12 col-lg-4 order-lg-3">
+          <div class="link-box link-box--submissions">
+            <div class="row">
+              <div class="col-6 col-lg-12 order-lg-2">
+                <h4 class="link-box-title link-box-title--submissions">Send in your content</h4>
+                <div class="link-box-text">Share exclusive content with us</div>
+                <a href="/submissions/" class="link-box-btn">Send in your content</a>
+              </div>
+              <div class="col-6 col-lg-12 order-lg-1">
+                <a href="/submissions/" class="opacity-hover"><div class="global-image-wrapper"><img src="@asset('images/lohan-1280x960.png')" class="img-fluid"/></div></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="page-footer_end">
+        <a href="#" class="t-c_link">Terms & Conditions</a>
+        <p class="copyright-text">© Pentzero 2018</p>
       </div>
     </div>
 
