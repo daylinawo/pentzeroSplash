c4cc442784e95ddf99a5999005793cb0143b4fd0
Franklin Mathurin <kainingem@gmail.com>
2018-07-04 00:54:17 +0100

    Style changes to Submissions page

--- site/web/app/themes/pentzero/resources/views/page-submissions.blade.php
+++ site/web/app/themes/pentzero/resources/views/page-submissions.blade.php
@@ -5,44 +5,37 @@
 <div class="page-header page-header-fpage">
 	@include('partials.page-header')
 </div>
-<div class="pform pform--submissions">
-	<div class="container">
-		@include('partials.form-header')
-		<div class="row">
-			<div class="col-md-6 col-xl-7">
-				<div class="pform-form">
-					<div class="pform-form-c">
-						<div class="pform-form--heading">
-							<h1 class="pform-form--title">Submit your content</h1>
-							<p class="pform-form--text">Fill out the form below to submit your content.</p>
-						</div>
-						<div class="pform-form-f">
-					  		<div class="pform-form--fwrap">
-					  			<? echo do_shortcode('[gravityform id="1" title="false" description="false"]'); ?>
-					  			<div class="clear"></div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-md-6 col-xl-5">
-				<div class="pform-side">
-					<div class="pform-side-c">
-						<h3 class="pform-side--title">More</h3>
-						<div class="pform-side--linkto">
-							<a href="/contact/" class="linkto-btn">Contact Us</a>
-							<div class="linkto-text">Please feel free to get in touch with us</div>
-						</div>
-				        <div class="fsignup-side">
-				            <div class="signup-wrapper">
-				            	<? echo do_shortcode('[mc4wp_form id="11"]'); ?>
-				            </div>
-				        </div>
+<div class="page-content">
+	<div class="page-content__container">
+		<div class="page-content__wrapper">
+			<div class="page-content__body">
+				<h1 class="page__title">Submissions</h1>
+				<div class="aform aform__submissions">
+					<p class="aform__text">Fill out the form below to submit your content. All fields are required so please ensure that none are left blank.</p>
+			  		<div class="aform__wrapper">
+			  			<? echo do_shortcode('[gravityform id="1" title="false" description="false"]'); ?>
+			  			<div class="clear"></div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+</div>
+
+<div class="page-footer page-footer__submissions">
+	<div class="page-footer__wrapper">
+		<img src="<?= get_theme_file_uri(); ?>/resources/assets/images/logo.png" class="page-footer__logo" />
+		<ul class="page-footer__links">
+			<li><a href="/index/">Home</a></li>
+			<li><a href="/submissions/">Submissions</a></li>
+			<li><a href="/contact/">Contact Us</a></li>
+		</ul>
+	    <div class="fsignup-footer">
+	        <div class="signup-wrapper">
+	        	<? echo do_shortcode('[mc4wp_form id="11"]'); ?>
+	        </div>
+	    </div>
+    </div>
 </div>
 
 @endsection
