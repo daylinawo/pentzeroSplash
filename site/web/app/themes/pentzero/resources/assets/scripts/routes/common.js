0bc05605fd2206e2124021ffb33e4f2fee06dbbd
Franklin Mathurin <kainingem@gmail.com>
2018-07-16 12:54:25 +0100

    added validation rules for contact us form

--- site/web/app/themes/pentzero/resources/assets/scripts/routes/common.js
+++ site/web/app/themes/pentzero/resources/assets/scripts/routes/common.js
@@ -1,29 +1,34 @@
 export default {
   init() {
-	var $form = $("#gform_1");
     var btn = $("#gform_submit_button_1");
+    var form = $('.gform-v');
 
-	$form.validate({
+	form.validate({
 		rules: {
 			input_12: {required: true},
 			input_2: {
 				required: true,
 				email: true,
 			},
-			input_7: {
+			input_3: {
 				required: true,
+				email: true,
 			},
+			input_7: {required: true},
+			input_4: {required: true},
 		},
 		tooltip_options: {
 			input_12: {placement:'right'},
 			input_2: {placement:'right'},
+			input_3: {placement:'right'},
 			input_7: {placement:'right'},
+			input_4: {placement:'right'},
 		},
 	});
 
 	btn.on('click', function(){
-		if($form.valid()){
-			$form.submit();
+		if(form.valid()){
+			form.submit();
 		}
 	});
 
